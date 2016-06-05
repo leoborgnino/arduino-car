255a094528ac6f04eaa309aa1564109f93b49a81
leoborgnino <leo.borgnino@gmail.com>
2016-06-05 19:02:11 -0300

    Se realizaron cambios en el metodo de envio. Tiene un parametro mas que es la direccion a la clase que se va a enviar los datos.

--- arduino-car.ino
+++ arduino-car.ino
@@ -22,11 +22,13 @@ char formlist[100];
 char data_rec[100];
 char mystring[30];
 char SerRx;
-int  respuestaid        =  0;
 int  MICRO_ADDR         = 10; //Verificar
 int  flag_uart          =  0;
 int  contador_backup    =  0;
 int  data_len_rx        =  0;
+int  id_received        =  0;
+int  graphic_address    =  0;
+int  planning_address   =  1;
 
 int   flag_accion       = 0;
 float valor_giro_temp   = 0;
@@ -45,7 +47,7 @@ int encoder1            = 0; //contadores encoders ruedas
 int encoder2            = 0;
 int grados_max          = 0;
 
-void send_uart(char*);
+void send_uart(char*,int);
 void receive_uart();
 void print_datos();
 void mover(int, int, int); //moverse hacia adelante x cm a y velocidad
@@ -58,7 +60,6 @@ void setup()
   Timer1.initialize(50000);              // Dispara cada 50 ms
   Timer1.attachInterrupt(ISR_Timer);     // Activa la interrupcion y la asocia a ISR_Blink
   Serial.begin(115200);                  // Inicio la transmision serie
-  Serial1.begin(115200, SERIAL_8N1);     
   analogWrite(PWM1, 127);                // PWM1 50% duty
   analogWrite(PWM2, 127);                // PWM2 50% duty
   attachInterrupt(0, ISR_INTE0, RISING); // Interrupcion externa en pin 2 por flanco de subida
@@ -67,56 +68,53 @@ void setup()
   calibrar_MPU6050(offset);
   obtener_datos(datos, offset);
   //send_uart("?!");
-  print_datos();
+  //print_datos();
   //Serial.print("\nMOVER\n");
   //mover(50,70,0);
   //Serial.print("GIRAR");
   //girar(180,1);
-  //for (int i = 0; i < 256; i++)
-  //{
-  //  Serial1.write(i);
-  //}
 }
 
 void loop()
 {
   if ((data_rec[0] == 'd') && (flag_accion))
   {
+    planning_address = id_received;
     mover(data_rec[1], data_rec[2], data_rec[3]);
     flag_accion = 0;
   }
   if ((data_rec[0] == 'f') && (flag_accion))
   {
+    planning_address = id_received;
     girar(data_rec[1], data_rec[2]);
     flag_accion = 0;
   }
   if ((data_rec[0] == 'g') && (flag_accion))
   {
+    graphic_address = id_received;
     obtener_datos(datos, offset);
     dtostrf(datos[0],5,2,buff[0]);
     dtostrf(datos[1],5,2,buff[1]);
     dtostrf(datos[2],5,2,buff[2]);
-    dtostrf(datos[3],5,2,buff[3]);
-    dtostrf(datos[4],5,2,buff[4]);
-    dtostrf(datos[5],5,2,buff[5]);
+    dtostrf(datos[4],5,2,buff[3]);
+    dtostrf(datos[5],5,2,buff[4]);
+    dtostrf(datos[6],5,2,buff[5]);
     sprintf(mystring, "%s %s %s %s %s %s !", buff[0], buff[1], buff[2], buff[3], buff[4], buff[5]);
     //sprintf(mystring,"%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f !",datos[0],datos[1],datos[2],datos[4],datos[5],datos[6]);
     //Serial.print(mystring);
-    send_uart(mystring);
+    send_uart(mystring,graphic_address);
     flag_accion = 0;
   }
 
   //CALIBRAR
   if ((data_rec[0] == 'h') && (flag_accion))
   {
-    calibrar_MPU6050(offset);
+    //calibrar_MPU6050(offset);
     flag_accion = 0;
   }
 
   if ((flag_timer) && (flag_rotacion))
   {
-    EIMSK |= (0 << INT0);
-    EIMSK |= (0 << INT1);
     flag_transmision = true;
     Wire.beginTransmission(0x68);
     Wire.write(0x47);                 // starting with register 0x3B (ACCEL_XOUT_H)
@@ -128,12 +126,12 @@ void loop()
     datos[6] = (datos[6] / 131) - offset[2];
     valor_giro_temp = valor_giro_temp + (datos[6] / 20);  
     
-    Serial.println(valor_giro_temp);
+    //Serial.println(valor_giro_temp);
     //print_datos();
     flag_timer = false;
   }
 
-  if (Serial1.available() > 0)
+  if (Serial.available() > 0)
   {
     receive_uart();
   }
@@ -143,8 +141,6 @@ void loop()
 
 void mover(int distancia, int vlc, int sentido)
 {
-  //EIMSK |= (1 << INT0);
-  //EIMSK |= (1 << INT1);
   if (!(sentido))
   {
     analogWrite(PWM1, 127 + vlc);
@@ -157,7 +153,7 @@ void mover(int distancia, int vlc, int sentido)
   }
   contador_movimiento = 0;
   distancia_max = distancia;
-  distancia_temp = 0;
+  distancia_temp = distancia_max + 1;
   flag_mover = true;
 
 }
@@ -191,7 +187,7 @@ void girar(int grados, int sentido) //0 antihorario 1 horario
 }
 
 
-void send_uart(char* data_send)
+void send_uart(char* data_send, int respuestaid)
 {
   int data_len    = 0;
   char caracter   = 0;
@@ -227,12 +223,12 @@ void send_uart(char* data_send)
   formlist[5 + data_len] = mod_iheader;
 
   for (int i = 0 ; i < data_len + 6; i++)
-    Serial1.write(formlist[i]);
+    Serial.write(formlist[i]);
 }
 
 void receive_uart()
 {
-  SerRx = Serial1.read();
+  SerRx = Serial.read();
   if ( ( flag_uart == 0 ) && ( (SerRx & 0xE0) == 160) ) //Encontramos el MIT
   {
     flag_uart++;
@@ -249,13 +245,11 @@ void receive_uart()
     flag_uart++;
   }
   else if ( ( SerRx == MICRO_ADDR ) && ( flag_uart == 3 ) )
-  {
     flag_uart++;
-  }
   else if ( flag_uart == 4 )
   {
-    respuestaid = SerRx;
     flag_uart++;
+    id_received = SerRx;
   }
   else if ( (flag_uart > 4) && (flag_uart < 5 + data_len_rx) )
   {
@@ -284,33 +278,21 @@ void print_datos()
 
 void ISR_Timer()
 {
-  //if (flag_mover)
-    //contador_movimiento++;
   if ( ( fabs(valor_giro_temp) > grados_max) && (flag_rotacion == true) )
     {
       analogWrite(PWM1, 127);
       analogWrite(PWM2, 127);
       flag_rotacion = false;
-      send_uart("ok!");
+      send_uart("ok!",planning_address);
       valor_giro_temp = 0;
-    }
-  
-  //if(contador_movimiento > distancia_max/2)
-  //  {
-  //    contador_movimiento = 0;
-  //    flag_mover = false;
-  //    analogWrite(PWM1, 127);
-  //    analogWrite(PWM2, 127);
-  //    send_uart("ok!");
-  //    
-  //  }
+    }  
   
   if ( ( distancia_temp > distancia_max) && (flag_mover == true) )
     {
       analogWrite(PWM1, 127);
       analogWrite(PWM2, 127);
       flag_mover = false;
-      send_uart("ok!");
+      send_uart("ok!",planning_address);
       distancia_temp = 0;
     }
   
